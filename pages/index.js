24cc0a86e9c3bf6758d047d9e4c293a67fe52175
Jason Lengstorf <jason@lengstorf.com>
2020-12-07 17:11:24 -0800

    feat: does this work?

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Next Image on Netlify</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Next Image on Netlify</h1>
+
+        <Image
+          src="/jason-rogers.jpg"
+          alt="Jason in the Mr. Rogers “I’m not very good at it” meme."
+          layout="responsive"
+          width="1368"
+          height="1044"
+        />
+      </main>
+    </div>
+  );
+}
